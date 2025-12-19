ece9ee3e6d1934216464c2e3ff3e4f42e855e19a
DenianHoerlle <deniancloss@gmail.com>
2025-12-19 11:52:55 -0300

    add NextRound problem

---
+++ src/NextRound.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class NextRound {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] firstLine = scan.nextLine().split(" ");
+
+        int n = Integer.parseInt(firstLine[0]);
+        int k = Integer.parseInt(firstLine[1]);
+
+        String[] contestants = scan.nextLine().split(" ");
+
+        int targetScore = Integer.parseInt(contestants[k]);
+
+        int winners = 0;
+
+        if (targetScore != 0) {
+            for (int i = 0; i < n; i++) {
+                int currentScore = Integer.parseInt(contestants[i]);
+                if  (currentScore >= targetScore) winners++;
+            }
+        }
+
+        System.out.println(winners);
+    }
+
+}
